91f5930d2b5f4a5ade0249ab5e4c567c785b6fb2
Håkan Nylén <confacted@gmail.com>
2015-12-12 23:41:17 +0100

    removed some lines and cleaned up a bit

--- app/services/citymapper_service.rb
+++ app/services/citymapper_service.rb
@@ -3,7 +3,7 @@ class CitymapperService
   include HTTParty
   base_uri 'citymapper.com'
 
-  #"https://citymapper.com/api/1/traveltime/?startcoord=51.525246%2C0.084672&endcoord=51.559098%2C0.074503&time=2014-11-06T19%3A00%3A02-0500&time_type=arrival&key="
+  # "https://citymapper.com/api/1/traveltime/?startcoord=51.525246%2C0.084672&endcoord=51.559098%2C0.074503&time=2014-11-06T19%3A00%3A02-0500&time_type=arrival&key="
 
   def initialize(apikey)
     @apikey = apikey
@@ -11,14 +11,14 @@ class CitymapperService
 
   ## get time between two cordinates and returns minutes
   def getTimeBetween(startcord, endcord, time)
-      options = {
-        :startcoord => startcord,
-        :endcoord => endcord,
-        :time => time,
-        :time_type => 'arrival',
-        :key => @apikey,
-        :format => "json"
-      }
-      self.class.get "/api/1/traveltime/?#{options.to_query}", {} #options don't work
+    options = {
+      startcoord: startcord,
+      endcoord: endcord,
+      time: time,
+      time_type: 'arrival',
+      key: @apikey,
+      format: 'json'
+    }
+    self.class.get "/api/1/traveltime/?#{options.to_query}", {} # options don't work
   end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,14 +1,13 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
   end
 
-  test "should get ajax" do
+  test 'should get ajax' do
     get :ajax
     assert_response :success
   end
-
 end
