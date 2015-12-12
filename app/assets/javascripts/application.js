9373674a7dfbec25125817a4f0c6666a6b2c36d3
Håkan Nylén <confacted@gmail.com>
2015-12-12 23:21:50 +0100

    using form for date and time
    and showing date and time of arrival instead of how long it takes in
    minutes.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,31 @@
 //= require picker/picker.time.js
 //= require_tree .
 $(function() {
+  function addMinutes(date, minutes) {
+    console.log(date.getTime());
+    console.log(minutes);
+    return new Date(date.getTime() + minutes*60000);
+  }
+
+  function pmamTo24(time)
+  {
+    var hours = Number(time.match(/^(\d+)/)[1]);
+    var minutes = Number(time.match(/:(\d+)/)[1]);
+    var AMPM = time.match(/\s(.*)$/)[1];
+    if(AMPM == "PM" && hours<12) hours = hours+12;
+    if(AMPM == "AM" && hours==12) hours = hours-12;
+    var sHours = hours.toString();
+    var sMinutes = minutes.toString();
+    if(hours<10) sHours = "0" + sHours;
+    if(minutes<10) sMinutes = "0" + sMinutes;
+    console.log(sHours + ":" + sMinutes);
+    return sHours + ":" + sMinutes;
+  }
+
+  //just helper to pick a data and time
+  $('#datepicker').pickadate();
+  $('#timepicker').pickatime();
+
   var path = new google.maps.MVCArray();
   var service = new google.maps.DirectionsService(), poly;
   var map = new google.maps.Map(document.getElementById('map_canvas'), {
@@ -48,22 +73,25 @@ $(function() {
     bounds.extend(rentifyMarker.getPosition());
     bounds.extend(evt.latLng);
     map.fitBounds(bounds);
-    $( "#current" )
-      .html( "Calculating Time.."); 
-    $.get( "/ajax", { start: "51.519952,-0.098424", end: evt.latLng.lat().toFixed(6)+","+evt.latLng.lng().toFixed(6), time: "2014-11-06T19:00:02-0500" }, function( data ) {
+    $( "#current" ).html( "Calculating Time..");
+    //create datetime
+    var date = document.getElementById("datepicker").value,
+        time = document.getElementById("timepicker").value
+    var datetime = new Date(date + " " + pmamTo24(time));
+    //calls the ajax in home_controller
+    $.get( "/ajax", { start: "51.519952,-0.098424", end: evt.latLng.lat().toFixed(6)+","+evt.latLng.lng().toFixed(6), timedate: datetime.toISOString() }, function( data ) {
       if(data.hasOwnProperty('travel_time_minutes')) {
         $( "#current" )
-          .html( "Minutes it will take: "+data.travel_time_minutes ); // John
+          .html( "You will be arriving: "+addMinutes(datetime,data.travel_time_minutes).toString() ); // John
       }
     else {
-      $( "#current" )
-        .html( data.error_message );
+      $( "#current" ).html( data.error_message );
     }}, "json" );
-      //document.getElementById('current').innerHTML = '<p>Marker dropped: Current Lat: ' + evt.latLng.lat().toFixed(3) + ' Current Lng: ' + evt.latLng.lng().toFixed(3) + '</p>';
   });
 
   google.maps.event.addListener(positionMarker, 'dragstart', function (evt) {
-      document.getElementById('current').innerHTML = '<p>Currently dragging marker...</p>';
+    $( "#current" )
+      .html( '<p>Currently dragging marker...</p>');
   });
 
   google.maps.event.addListener(rentifyMarker, 'click', function() {
